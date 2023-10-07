c3ea3a78c4a2d0cbf53d016dc77434aaed865df6
tjdnjjangjjang <seowoo@letscoding.kr>
2023-10-07 11:52:43 +0900

    앱 수정하기

--- edu/views.py
+++ edu/views.py
@@ -1,3 +1,8 @@
+from django.views.generic import View
 from django.shortcuts import render
 
-# Create your views here.
+class Index(View):
+    template_name = 'index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
